b542b17ffbde29cf5f2a01d4f639e6cc9e2ce97c
Brian Santos <brianmsantos81@gmail.com>
2020-02-07 16:38:33 -0800

    added some explanations to existing questions

--- Chapter_2_Program_Structure/main.js
+++ Chapter_2_Program_Structure/main.js
@@ -14,7 +14,13 @@
 for (let hash = "#"; hash.length < 8; hash += "#")
 console.log(hash);
 
-//Fizzbuzz from 1-100
+// //Fizzbuzz from 1-100
+// Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+// When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+
+//1st setup the index, check the condition, and update the index(1, less than 100, and increment by 1)
+//2nd needs a conditional statemment to check which numbers are divisible by 3, 5, and both 3 and 5
+//3rd anything that is not divisible by 3, 5, or both must console log the number using concatenation
 
 for (let i = 1; i < 100; i++) {
   if(i % 3 === 0 && i % 5 === 0){
@@ -28,11 +34,18 @@ for (let i = 1; i < 100; i++) {
   }
 }
 
-// for (var n = 1; n <= 100; n++) {
-//   var output = "";
-//   if (n % 3 == 0)
-//     output += "Fizz";
-//   if (n % 5 == 0)
-//     output += "Buzz";
-//   console.log(output || n);
-// }
+// Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
+// Passing this string to console.log should show something like this:
+//  # # # #
+// # # # # 
+//  # # # #
+// # # # # 
+//  # # # #
+// # # # # 
+//  # # # #
+// # # # #
+
+let hash = "#"
+let space = " "
+let line = 8
+
