da3c3910ef1e9c3b0b9227037e0855820aa5f4e4
Brian Santos <brianmsantos81@gmail.com>
2020-02-13 11:49:08 -0800

    recursion soliution added to recursion problem, both non-recursion and recursion solutions posted

--- Chapter_3_Functions/main.js
+++ Chapter_3_Functions/main.js
@@ -6,6 +6,10 @@ function returnMin (a, b){
   return a<b ? a:b;
 };
 
+console.log(returnMin(0, 10));
+console.log(returnMin(0, -10));
+
+
 // Recursion
 // We’ve seen that % (the remainder operator) can be used to test whether a number is even or odd by using % 2 to see whether it’s divisible by two. Here’s another way to define whether a positive whole number is even or odd:
 
@@ -17,8 +21,7 @@ function returnMin (a, b){
 
 // Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?
 
-console.log (returnMin(1,2))
-
+//NON-RECURSIVE SOLUTION
 function isEven (n){
   if(n%2 ==0){
   return true
@@ -32,3 +35,24 @@ console.log(isEven(40))
 console.log(isEven(65))
 
 console.log(isEven(-1))
+
+//RECURSIVE SOLUTION
+function even(n) {
+    if (n == 0) return true;
+    else if (n == 1) return false;
+    else if (n < 0 ) return even(-n);
+    else return even(n - 2);
+  };
+
+  console.log(even(50));
+  
+  console.log(even(75));
+
+  console.log(even(-1));
+ 
+  // Bean counting
+  // You can get the Nth character, or letter, from a string by writing "string"[N]. The returned value will be a string containing only one character (for example, "b"). The first character has position 0, which causes the last one to be found at position string.length - 1. In other words, a two-character string has length 2, and its characters have positions 0 and 1.
+  
+  // Write a function countBs that takes a string as its only argument and returns a number that indicates how many uppercase “B” characters there are in the string.
+  
+  // Next, write a function called countChar that behaves like countBs, except it takes a second argument that indicates the character that is to be counted (rather than counting only uppercase “B” characters). Rewrite countBs to make use of this new function.
