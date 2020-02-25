d0eb8dffd6590c2c94609c54e19bf053b05ac9cb
Brian Santos <brianmsantos81@gmail.com>
2020-02-25 15:48:00 -0800

    exercise for chapter 4 started for range function

---
+++ Chapter_4_Data_Structures_Objects_and_Arrays/main.js
@@ -0,0 +1,7 @@
+// Write a range function that takes two arguments, start and end, and returns an array containing all the numbers from start up to (and including) end.
+
+// Next, write a sum function that takes an array of numbers and returns the sum of these numbers. Run the example program and see whether it does indeed return 55.
+
+function range(start, end){
+  return
+}
